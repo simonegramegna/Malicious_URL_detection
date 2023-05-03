63151af72a71bff09780784b96d220c1080729cc
simo_lenovo <gramegna.simone@gmail.com>
2023-05-03 18:55:25 +0200

    fix

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import pandas as pd
 from mining import *
 import os
 
-dataset_dir = os.path.dirname(os.path.abspath(__file__)) + "\datasets" + "\malicious_urls.csv"
+dataset_folder = "\datasets"
+dataset_file = "\malicious_urls.csv"
+
+print(os.name)
+
+if os.name != 'nt':
+    dataset_folder = "/datasets"
+    dataset_file = "/malicious_urls.csv"
+
+dataset_dir = os.path.dirname(os.path.abspath(__file__)) + dataset_folder + dataset_file
 df = pd.read_csv(dataset_dir)
 
 print(df.head())
